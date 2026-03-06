79d34cd318c4c749ac423bc803efb40f72167e76
Mauricio Gaona <edgarmau226@gmail.com>
2026-03-06 15:54:20 -0600

    Preparar deploy sin credenciales sensibles

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,18 +1,14 @@
-// api/firebaseAdmin.js
 import admin from "firebase-admin";
-import fs from "fs";
 
-// Leer service account
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
-// Inicializar Firebase Admin SOLO una vez
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
-// Exportar Firestore y Auth
+export default admin;
 export const db = admin.firestore();
 export const authAdmin = admin.auth();
 export const FieldValue = admin.firestore.FieldValue;
